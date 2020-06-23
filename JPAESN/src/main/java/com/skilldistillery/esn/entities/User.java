281783d49b2eb6cef1aad6afc6340bcc0b23510d
darinthompson <darint07@gmail.com>
2020-06-23 12:40:30 -0500

    stubbed out user

--- JPAESN/src/main/java/com/skilldistillery/esn/entities/User.java
+++ JPAESN/src/main/java/com/skilldistillery/esn/entities/User.java
@@ -1,5 +1,72 @@
 package com.skilldistillery.esn.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class User {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String userName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public User() {}
+	public User(int id, String userName) {
+		super();
+		this.id = id;
+		this.userName = userName;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=");
+		builder.append(id);
+		builder.append(", userName=");
+		builder.append(userName);
+		builder.append("]");
+		return builder.toString();
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 
 }
