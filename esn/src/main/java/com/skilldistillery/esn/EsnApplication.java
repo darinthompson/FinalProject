c837c3af00d1bdbddf60f95c7449bbf3e30bd98f
darinthompson <darint07@gmail.com>
2020-06-23 12:45:26 -0500

    added spring project

---
+++ esn/src/main/java/com/skilldistillery/esn/EsnApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.esn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsnApplication.class, args);
+	}
+
+}
