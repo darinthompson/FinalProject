cbb1b38127aab0ee7ae5700c798d4b234fe86d9d
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-06-23 14:11:39 -0600

    Add @CrossOrigin to TestController

--- esn/src/main/java/com/skilldistillery/esn/controllers/TestController.java
+++ esn/src/main/java/com/skilldistillery/esn/controllers/TestController.java
@@ -1,11 +1,13 @@
 package com.skilldistillery.esn.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({"*", "http:localhost:4209"})
 public class TestController {
 
 	@GetMapping("ping")
