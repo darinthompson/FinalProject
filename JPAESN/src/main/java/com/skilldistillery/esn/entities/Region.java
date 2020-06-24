3d83a08ad8b6ba14352e9cbfb391a6d84fcbbdff
darinthompson <darint07@gmail.com>
2020-06-24 11:53:47 -0500

    built out entities and tests

--- JPAESN/src/main/java/com/skilldistillery/esn/entities/Region.java
+++ JPAESN/src/main/java/com/skilldistillery/esn/entities/Region.java
@@ -1,5 +1,79 @@
 package com.skilldistillery.esn.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Region {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;	
+	private String name;
+
+	
+	public Region() {}
+	public Region(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Region [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Region other = (Region) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
 }
---
+++ JPAESN/src/test/java/com/skilldistillery/esn/entities/RegionTest.java
@@ -0,0 +1,46 @@
+package com.skilldistillery.esn.entities;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegionTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Region region;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("EsportsPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		region = em.find(Region.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		region = null;
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
