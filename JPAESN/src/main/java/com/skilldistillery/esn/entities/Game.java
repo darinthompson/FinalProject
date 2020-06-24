e66e409bf28fd50c1207b27448208540e43c83fb
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-06-24 11:25:32 -0600

    Build entities: Game, GameStats, Organization, Player, PlayerMatchStats, Series, SeriesMatch

--- JPAESN/src/main/java/com/skilldistillery/esn/entities/Game.java
+++ JPAESN/src/main/java/com/skilldistillery/esn/entities/Game.java
@@ -1,5 +1,111 @@
 package com.skilldistillery.esn.entities;
 
-public class Game {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Game {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String genre;
+	@Column(name = "img_url")
+	private String imgURL;
+	@Column(name = "website_url")
+	private String websiteURL;
+	
+	public Game() {}
+
+	public Game(int id, String title, String genre, String imgURL, String websiteURL) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+		this.imgURL = imgURL;
+		this.websiteURL = websiteURL;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getImgURL() {
+		return imgURL;
+	}
+
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+
+	public String getWebsiteURL() {
+		return websiteURL;
+	}
+
+	public void setWebsiteURL(String websiteURL) {
+		this.websiteURL = websiteURL;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Game [id=");
+		builder.append(id);
+		builder.append(", title=");
+		builder.append(title);
+		builder.append(", genre=");
+		builder.append(genre);
+		builder.append(", imgURL=");
+		builder.append(imgURL);
+		builder.append(", websiteURL=");
+		builder.append(websiteURL);
+		builder.append("]");
+		return builder.toString();
+	}
+	
 }
