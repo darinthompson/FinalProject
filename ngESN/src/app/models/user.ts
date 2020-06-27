d5d481255558faf18ce1092beb73c3dfae89bcb4
Shane Duncan <aequitas321@icloud.com>
2020-06-27 15:08:29 -0400

    added user model and constructor

--- ngESN/src/app/models/user.ts
+++ ngESN/src/app/models/user.ts
@@ -1,2 +1,20 @@
+import {Profile} from "./profile";
+
 export class User {
+
+  id: number;
+  username: string;
+  password: string;
+  enabled: boolean;
+  role: string;
+  profile: Profile;
+
+  constructor(id?: number, username?: string, password?: string, enabled?: boolean, role?: string, profile?: Profile) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.enabled = enabled;
+    this.role = role;
+    this.profile = profile;
+  }
 }
