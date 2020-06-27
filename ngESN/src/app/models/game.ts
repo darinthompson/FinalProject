b13c43ec3691b58096b6c0ef0b4d222bd2672475
Shane Duncan <aequitas321@icloud.com>
2020-06-27 13:47:31 -0400

    added game model and constructor

--- ngESN/src/app/models/game.ts
+++ ngESN/src/app/models/game.ts
@@ -1,3 +1,6 @@
+import {GameStat} from "./game-stat";
+import { Team } from './team';
+
 export class Game {
 
   id: number;
@@ -5,5 +8,16 @@ export class Game {
   genre: string;
   imgURL: string;
   websiteURL: string;
-  gameStat:
+  gameStat: GameStat[];
+  teams: Team[];
+
+  constructor(id?: number, title?: string, genre?: string, imgURL?: string, websiteURL?: string, gameStat?: GameStat[], teams?: Team[]) {
+    this.id = id;
+    this.title = title;
+    this.genre = genre;
+    this.imgURL = imgURL;
+    this.websiteURL = websiteURL;
+    this.gameStat = gameStat;
+    this.teams = teams;
+  }
 }
