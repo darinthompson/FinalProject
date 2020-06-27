69adcb68e8eab143c01c221029d3b535bd879f31
darinthompson <darint07@gmail.com>
2020-06-27 13:11:39 -0500

    completed series model

--- ngESN/src/app/models/series.ts
+++ ngESN/src/app/models/series.ts
@@ -1,2 +1,17 @@
+import { SeriesMatch } from './series-match';
+
 export class Series {
+  id: number;
+  name: string;
+  description: string;
+  imgURL: string;
+  seriesMatch: SeriesMatch[];
+
+  constructor(id?: number, name?: string, description?: string, imgURL?: string, seriesMatch?: SeriesMatch[]) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.imgURL = imgURL;
+    this.seriesMatch = seriesMatch;
+  }
 }
