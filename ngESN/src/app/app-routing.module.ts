0f51ca8eb9718126ec148d58c70716840da725b3
darinthompson <darint07@gmail.com>
2020-06-27 16:58:23 -0500

    stubbed out basic ngRoutes

--- ngESN/src/app/app-routing.module.ts
+++ ngESN/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ArticleComponent } from './components/article/article.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { GameComponent } from './components/game/game.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'article', component: ArticleComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'game', component: GameComponent},
+  {path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
