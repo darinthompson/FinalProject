f76f4ab209b1ff2c98c259d4350d3075581f567a
Shane Duncan <aequitas321@icloud.com>
2020-06-28 18:09:59 -0400

    added on click for games to route to id

--- ngESN/src/app/app-routing.module.ts
+++ ngESN/src/app/app-routing.module.ts
@@ -1,27 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { ArticleComponent } from './components/article/article.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { GameComponent } from './components/game/game.component';
-import { RegisterComponent } from './components/register/register.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { SeriesMatchComponent } from './components/series-match/series-match.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {ArticleComponent} from './components/article/article.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {GameComponent} from './components/game/game.component';
+import {RegisterComponent} from './components/register/register.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {SeriesMatchComponent} from './components/series-match/series-match.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'article', component: ArticleComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'game', component: GameComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'match', component: SeriesMatchComponent },
-  { path: '', component: HomeComponent },
-  { path: '**', component: NotFoundComponent }
+  {path: 'home', component: HomeComponent},
+  {path: 'article', component: ArticleComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'game', component: GameComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'match', component: SeriesMatchComponent},
+  {path: 'games/:id', component: GameComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
