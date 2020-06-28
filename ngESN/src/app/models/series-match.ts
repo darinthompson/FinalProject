88e87b90bf658dd5db16693d644e22cfded60492
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-06-28 15:00:42 -0600

    Commented out error in home.component.html, update profile page, minor formatting

--- ngESN/src/app/models/series-match.ts
+++ ngESN/src/app/models/series-match.ts
@@ -1,9 +1,8 @@
-import {Time} from "@angular/common";
-import {Series} from "./series";
-import {Team} from "./team";
+import { Time } from '@angular/common';
+import { Series } from './series';
+import { Team } from './team';
 
 export class SeriesMatch {
-
   id: number;
   title: string;
   team1Title: string;
@@ -15,13 +14,25 @@ export class SeriesMatch {
   team2: Team;
   winner: Team;
 
-  constructor(id?: number, title?: string, team1Title?: string, team2Title?: string, startDate?: Date, startTime?: Time, series?: Series, team1?: Team, team2?: Team, winner?: Team) {
+  constructor(
+    id?: number,
+    title?: string,
+    team1Title?: string,
+    team2Title?: string,
+    startDate?: Date,
+    startTime?: Time,
+    series?: Series,
+    team1?: Team,
+    team2?: Team,
+    winner?: Team
+  ) {
     this.id = id;
     this.title = title;
     this.team1Title = team1Title;
     this.team2Title = team2Title;
     this.startDate = startDate;
-    this.startTime =startTime;
+    this.startTime = startTime;
+    this.series = series;
     this.team1 = team1;
     this.team2 = team2;
     this.winner = winner;
