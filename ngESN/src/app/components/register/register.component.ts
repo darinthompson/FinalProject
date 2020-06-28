20f7d1d47befce52777d4b6fccdb27dab6ee0dba
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-06-27 18:50:32 -0600

    Remove register service, added auth service, got user registration working

--- ngESN/src/app/components/register/register.component.ts
+++ ngESN/src/app/components/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { ProfileService } from 'src/app/services/profile.service';
+import { Profile } from 'src/app/models/profile';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  newUser: User = new User();
+
+  constructor(
+    private auth: AuthService,
+    private profileSvc: ProfileService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  register(user: User) {
+    this.auth.register(user).subscribe(
+      regSuccess => {
+        console.log('User ' + regSuccess + ' created');
+        this.newUser = new User();
+      },
+      regFail => {
+        console.error('RegisterComponent.register(): Error on register');
+        console.error(regFail);
+        this.router.navigateByUrl('fourohfour')
+      }
+    );
+  }
+
+  createProfile(form: NgForm) {
+    const profile: Profile = form.value;
+    console.log(profile);
+    this.profileSvc.create(profile).subscribe(
+      createSuccess => {
+        this.auth.login(this.newUser.username, this.newUser.password).subscribe(
+          logSuccess => {
+            this.router.navigateByUrl('home');
+            console.log(logSuccess);
+          },
+          logFail => {
+            console.error('AuthService.login(): Error logging in');
+            console.error(logFail);
+            this.router.navigateByUrl('fourohfour')
+          }
+        );
+      },
+      createFail => {
+        console.error('RegisterComponent.createProfile(): Error on profile creation');
+        console.error(createFail);
+        this.router.navigateByUrl('fourohfour')
+      }
+    );
   }
 
 }
--- ngESN/src/app/services/profile.service.ts
+++ ngESN/src/app/services/profile.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { Profile } from '../models/profile';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private url = environment.baseURL + 'api/profiles';
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  create(profile: Profile) {
+    const httpOptions = this.getHttpOptions();
+
+    // if (!this.auth.checkLogin()) {
+    //   this.router.navigateByUrl('login');
+    // }
+
+    return this.http.post<Profile>(this.url, profile, httpOptions).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError('ProfileService.create(): Error creating profile: ' + err);
+      })
+    );
+  }
+
+  getHttpOptions() {
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest',
+      }),
+    };
+    return httpOptions;
+  }
 }
