83abcd1daa08c76c8f0168452a47ecf6e11c8a01
darinthompson <darint07@gmail.com>
2020-06-29 17:45:29 -0500

    Can add comments, and form resets after submission

--- esn/src/main/java/com/skilldistillery/esn/repositories/CommentRepo.java
+++ esn/src/main/java/com/skilldistillery/esn/repositories/CommentRepo.java
@@ -8,7 +8,6 @@ import com.skilldistillery.esn.entities.Comment;
 
 public interface CommentRepo extends JpaRepository<Comment, Integer> {
 
-	List<Comment> findByProfile_Id(Integer pid);
-	
+	List<Comment> findByProfile_Id(Integer pid);	
 	List<Comment> findByArticle_Id(Integer aid);
 }
