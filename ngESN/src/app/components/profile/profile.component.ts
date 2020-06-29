929996e4d3c5cd453215f7e4259cf2a00a9a2170
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-06-29 00:09:31 -0600

    Add getUsername() and getProfile() to ProfileComponent and ProfileService

--- ngESN/src/app/components/profile/profile.component.ts
+++ ngESN/src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
+import { Profile } from 'src/app/models/profile';
 
 @Component({
   selector: 'app-profile',
@@ -8,12 +9,33 @@ import { ProfileService } from 'src/app/services/profile.service';
 })
 export class ProfileComponent implements OnInit {
 
+  userProfile: Profile;
+  username: string;
+
   constructor(
     private profileService: ProfileService
   ) { }
 
   ngOnInit(): void {
+    this.getUsername();
+    this.getProfile();
   }
 
+  getUsername() {
+    this.username = localStorage.getItem('username');
+  }
+
+  getProfile() {
+    this.profileService.getByUsername().subscribe(
+      profile => {
+        console.log(profile);
+        this.userProfile = profile;
+      },
+      fail => {
+        console.error('ProfileComponent.getProfile(): Error retrieving profile:');
+        console.error(fail);
+      }
+    );
+  }
 
 }
