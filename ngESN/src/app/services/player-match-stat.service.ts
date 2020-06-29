570678aaa97f41f20c3a204f2e3380783f788cd5
Shane Duncan <aequitas321@icloud.com>
2020-06-29 18:37:11 -0400

    working on match stats page

--- ngESN/src/app/services/player-match-stat.service.ts
+++ ngESN/src/app/services/player-match-stat.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Profile} from "../models/profile";
+import {catchError} from "rxjs/operators";
+import {throwError} from "rxjs";
+import {PlayerMatchStat} from "../models/player-match-stat";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerMatchStatService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private url = environment.baseURL + 'api/stats';
+
+
+  getStatByPlayerAndMatch(playerId: number, matchId: number){
+    return this.http.get<PlayerMatchStat[]>(`${this.url}/match/${matchId}/player/${playerId}`).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+          ' Error retrieving Match Stats: ' + err
+        );
+      })
+    );
+  }
 }
