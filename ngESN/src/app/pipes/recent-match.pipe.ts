3793348f6f053ec7c75c6539ab8477cc43eccf09
Shane Duncan <aequitas321@icloud.com>
2020-06-30 13:48:06 -0400

    working match pipe

--- ngESN/src/app/pipes/recent-match.pipe.ts
+++ ngESN/src/app/pipes/recent-match.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {SeriesMatch} from "../models/series-match";
 
 @Pipe({
@@ -7,13 +7,32 @@ import {SeriesMatch} from "../models/series-match";
 export class RecentMatchPipe implements PipeTransform {
 
   transform(matches: SeriesMatch[], howMany: number = 2): SeriesMatch[] {
-    matches.sort((m1,m2) => {
+    console.log(matches);
+    matches.sort((m1, m2) => {
       let gameOrder = m1.team1.game.id - m2.team1.game.id;
-      if (gameOrder === 0){
-        
+      if (gameOrder === 0) {
+        gameOrder = m2.startDate.localeCompare(m1.startDate);
       }
+      return gameOrder;
     });
-    return null;
+    console.log(matches);
+    const result: SeriesMatch[] = [];
+    let gameFound = 0;
+    let currentGame = matches[0].team1.game.id;
+    for (let match of matches) {
+      if (match.team1.game.id === currentGame) {
+        if (gameFound++ < howMany) {
+          result.push(match);
+        } else {
+          continue;
+        }
+      }else{
+        currentGame = match.team1.game.id;
+        gameFound =1;
+        result.push(match);
+      }
+    }
+    return result;
   }
 
 }
